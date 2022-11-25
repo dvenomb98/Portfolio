cb015c6f6ec344cf75e36bfcdecbdf5a087cb347
Daniel Bílek <daniel.bilek@regiojet.cz>
2022-11-25 15:31:36 +0100

    fixed hyd ration error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
