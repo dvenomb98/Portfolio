b14542fe9a044a90caaf4c2ef602732a35fe6e50
Daniel Bílek <daniel.bilek@regiojet.cz>
2022-11-25 15:12:03 +0100

    fix more theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-theme';
+import { ThemeProvider } from 'next-themes';
 import React from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
